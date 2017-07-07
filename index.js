ec219e2bb15c3531b70fd2e648b946d4a80336e4
Alex Mathews <alexjmathews@gmail.com>
2017-07-07 15:56:10 -0400

    basic encoding method

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const { spawn } = require('child_process');
 
 
-function test() {
-	return 'hello';
+function encode(input, output, callback) {
+	if (!callback) {
+		throw new Error('No function given for callback');
+	}
+	const ls = spawn('./build/draco_encoder', ['-i', input, '-o', output]);
+	const stderr = [];
+	const stdout = [];
+
+	ls.stdout.on('data', (data) => {
+		stdout.push(data);
+	});
+
+	ls.stderr.on('data', (data) => {
+		stderr.push(data);
+	});
+
+	ls.on('close', ((code) => {
+		if (code > 0) {
+			return callback(stderr, stdout);
+		}
+		return callback(null, stdout);
+	}));
 }
 
+
 module.exports = {
-	test
+	encode
 };
